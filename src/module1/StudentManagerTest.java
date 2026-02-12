2d4d3c0d53d9176be9772debb119bcf3079ea062
SejalNilesh <sejalchavan479@gmail.com>
2026-02-12 14:03:48 +0530

    Added additional JUnit test cases for StudentManagerLegacy

--- src/module1/StudentManagerTest.java
+++ src/module1/StudentManagerTest.java
@@ -1,12 +1,71 @@
+// package src.module1;
+// import static org.junit.jupiter.api.Assertions.*;
+// import org.junit.jupiter.api.Test;
+
+// public class StudentManagerTest {
+
+//     @Test
+//     void testCreate() {
+//         StudentManagerLegacy sm = new StudentManagerLegacy();
+//         assertNotNull(sm);
+//     }
+// }
+
+
 package src.module1;
+
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class StudentManagerTest {
 
+    StudentManagerLegacy sm;
+
+    @BeforeEach
+    void setUp() {
+        sm = new StudentManagerLegacy();
+    }
+
     @Test
     void testCreate() {
-        StudentManagerLegacy sm = new StudentManagerLegacy();
         assertNotNull(sm);
     }
+
+    @Test
+    void testAddStudent() {
+        assertDoesNotThrow(() -> {
+            sm.addStudent("101", "Sejal", "CS");
+        });
+    }
+
+    @Test
+    void testMultipleStudents() {
+        assertDoesNotThrow(() -> {
+            sm.addStudent("201", "A", "IT");
+            sm.addStudent("202", "B", "IT");
+        });
+    }
+
+    @Test
+    void testEmptyName() {
+        assertDoesNotThrow(() -> {
+            sm.addStudent("102", "", "CS");
+        });
+    }
+
+    @Test
+    void testEmptyId() {
+        assertDoesNotThrow(() -> {
+            sm.addStudent("", "Test", "CS");
+        });
+    }
+
+    // @Test
+    // void testFailExample() {
+    //         assertThrows(Exception.class, () -> {
+    //         sm.addStudent("101", "Sejal", "CS");
+    //     });
+    // }
+
 }
